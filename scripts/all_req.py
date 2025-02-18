84ec16d0743fae20e0857ea41ca8efc31377f8bb
jtorre-netquest <juan.manueldelatorresanchez@nielseniq.com>
2025-02-18 02:36:21 -0500

    adding new script and jenkinsfile

---
+++ scripts/all_req.py
@@ -0,0 +1,48 @@
+import requests
+import os
+import sys
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+def do_req(url, proxy=None):
+    try:
+        if proxy:
+            proxies = {
+                "http": proxy,
+                "https": proxy
+            }
+            response = requests.get(url, proxies=proxies, stream=True)
+        else:
+            response = requests.get(url, stream=True)
+
+        return response.status_code
+    except Exception as e:
+        return None
+
+
+try:
+
+    if len(sys.argv) != 2:
+        print("Use: python main.py <proxy_host:port>")
+        sys.exit(1)
+
+    proxy = sys.argv[1]
+    proxy = "http://" + proxy
+
+    file_name = "hosts.reduced"
+    with open(file_name, "r") as f:
+        urls = [line.strip() for line in f if line.strip()]
+
+    total_urls = len(urls)
+    total_200 = 0
+
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        futures = {executor.submit(do_req, url, proxy): url for url in urls}
+        for future in as_completed(futures):
+            status = future.result()
+            if status == 200:
+                total_200 += 1
+
+    print(f"Total URLs: {total_urls}")
+    print(f"Total 200: {total_200}")
+except Exception as e:
+    print(f"An error occurred: {e}")
