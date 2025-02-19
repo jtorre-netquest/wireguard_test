1b8efb4cd7a2cbe4c0e2b51f477b78acf0e82617
jtorre-netquest <juan.manueldelatorresanchez@nielseniq.com>
2025-02-19 10:28:56 -0500

    filename fix

--- scripts/all_req.py
+++ scripts/all_req.py
@@ -23,7 +23,8 @@ try:
     proxy = sys.argv[1]
     proxy = "http://" + proxy
 
-    file_name = os.path.abspath("./hosts.reduced")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_name = os.path.join(script_dir, 'hosts.reduced')
     print(file_name)
     with open(file_name, "r") as f:
         urls = [line.strip() for line in f if line.strip()]
