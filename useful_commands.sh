0d71f3172c37384ec2bb8ee5007a98ad7352d437
jtorre-netquest <jtorre@netquest.com>
2025-05-19 19:12:14 -0500

    updating useful commands

--- useful_commands.sh
+++ useful_commands.sh
@@ -2,6 +2,8 @@ docker network create \
   --subnet=192.168.100.0/24 \
   wireguard-net
 
+# SERVER
+
 docker run -d \
   --name=wg-server \
   --cap-add=NET_ADMIN \
@@ -13,22 +15,15 @@ docker run -d \
   -e SERVERPORT=51820 \
   -e PEERS=1 \
   -e ALLOWEDIPS=0.0.0.0/0 \
-  -v wg-server-config:/config \
   --sysctl="net.ipv4.conf.all.src_valid_mark=1" \
   -p 51820:51820/udp \
   lscr.io/linuxserver/wireguard:latest
 
 docker exec wg-server cat /config/peer1/peer1.conf
 
-docker run -d \
-  --name=wg-client \
-  --cap-add=NET_ADMIN \
-  --network=wireguard-net \
-  -v $(pwd)/wireguard_confs/client.conf:/config/wg_confs/wg0.conf \
-  --sysctl="net.ipv4.conf.all.src_valid_mark=1" \
-  lscr.io/linuxserver/wireguard:latest
+# CLIENT
 
-docker container stop wg-client && docker container rm wg-client
+docker build -t test-wireguard:latest .
 
 docker run -d \
   --name=wg-client \
@@ -44,4 +39,7 @@ docker run --name=wg-client \
     -v $(pwd)/main.py:/app/main.py \
     -v $(pwd)/wg.conf:/etc/wireguard/wg0.conf \
     --sysctl="net.ipv4.conf.all.src_valid_mark=1" \
-    test-wireguard:custom python /app/main.py
+    test-wireguard:latest python /app/main.py wg-client:51820
+
+docker container stop wg-client && docker container rm wg-client
+sudo docker container stop wg-client && sudo docker container rm wg-client
